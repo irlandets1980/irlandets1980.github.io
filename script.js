afa10a26f3baa02336ba0eb81c1e46b96b4be16e
irlandets1980 <irlandets1980@gmail.com>
2020-01-18 18:17:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// let sum = 2+3;
+
+// let arr = ['plum.png','orange.bmp'];
+
+// console.log('сумма равна = '+sum);
+// let answer = +prompt('вы уверены?','да');
+// console.log(answer);
+let incr = 10,
+descr = 10;
