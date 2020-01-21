42e2321d37eb7bcc0a6429f76a67d92fcdf94179
irlandets1980 <irlandets1980@gmail.com>
2020-01-21 18:21:24 +0300

    zadanie2.2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?");
+let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-    budget: money,//бюджет
+    budget: money, //бюджет
     timeData: time,
-    expenses: {},//расход
+    expenses: {}, //расход
     optionalExpenses: {},
     income: [],
     savings: false
@@ -23,12 +23,23 @@ for (let i = 0; i < 2; i++) {
     if ((typeof (a)) === "string" && (typeof (a)) != null && (typeof (b)) != null && a != "" && b != "" && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
-    } else {
-
+    } else {                            
+        console.log ("bad result");
+        i--;
     }
 
 };
-appData.manyPerDay=appData.budget / 30;
+appData.manyPerDay = appData.budget / 30;
 
 alert("Ежедневный бюджет : " + appData.budget / 30);
 
+if (appData.manyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.manyPerDay > 100 && appData.manyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.manyPerDay > 200) {
+    console.log('Высокий уровень достатка');
+
+}else{
+    console.log('Произошла ошибка');
+}
