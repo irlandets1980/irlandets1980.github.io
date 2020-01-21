6fa7a698c09da510dd614d16efad9294aabaf76e
Виктор My <Chief@Chief.rgmu.info>
2020-01-21 18:00:07 +0300

    homework_2

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-
-let appData = {
-    budget: money,//бюджет
-    timeData: time,
-    expenses: {},//расход
-    optionalExpenses: {},
-    income: [],
-    savings: false
-
-}; 
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
-let answer2 = prompt("Во сколько обойдется?");
-let answer3 = prompt("Введите обязательную статью расходов в этом месяце");
-let answer4 = prompt("Во сколько обойдется?");
-appData.expenses = {  //заполняем расходы
-    answer1: answer2,
-    answer3: answer4
-};
-
-
-alert("бюджет на 1 день: "+appData.budget/30);
+// let num = 50;
+// switch (num) {
+//     case num < 49:
+//         console.log("Мало!")
+//         break;
+//     case num > 100:
+//         console.log("Много!")
+//         break;
+//     case  50:
+//         console.log("Равно!")
+//         break;
+//     default:
+//         console.log();
+//         break;
+// }
 
